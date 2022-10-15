25de15091cec8f13689e2c96c8cb66ae6952f4c8
Kary Lau <00957055@ntou.edu.tw>
2022-10-15 15:43:28 +0800

    init commit

---
+++ FirstClass.java
@@ -0,0 +1,12 @@
+// 這是你這堂課的第一支Java程式
+public class FirstClass
+
+{
+    // main method begins execution of Java application
+    public static void main( String args[] )
+    {
+        System.out.println( "這是我的第一支Java程式!" );
+        System.out.println( "學號:\"00957055\"\n姓名:\"劉展喬\"");
+} // end method main
+
+} // end class FirstClass
